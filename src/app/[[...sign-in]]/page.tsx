464a78ddeccbffcd0754edf8693048f52d256593
Ashutosh9110 <ashusingh19911082@gmail.com>
2025-01-15 14:01:02 +0530

    added keys in .env

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -6,7 +6,7 @@ import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-  
+
 const LoginPage = () => {
   const { isLoaded, isSignedIn, user } = useUser();
 
@@ -29,7 +29,7 @@ const LoginPage = () => {
         >
           <h1 className="text-xl font-bold flex items-center gap-2">
             <Image src="/logo.png" alt="" width={24} height={24} />
-            School Management
+            SchooLama
           </h1>
           <h2 className="text-gray-400">Sign in to your account</h2>
           <Clerk.GlobalError className="text-sm text-red-400" />
